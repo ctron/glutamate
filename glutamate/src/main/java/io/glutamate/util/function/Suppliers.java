1f7808f2c631a3ea7bd2f64afffa8c7e48a051ba
Jens Reimann <jreimann@redhat.com>
2017-10-10 09:34:24 +0200

    Add Supplier mapper

--- glutamate/src/main/java/io/glutamate/util/function/Suppliers.java
+++ glutamate/src/main/java/io/glutamate/util/function/Suppliers.java
@@ -10,8 +10,10 @@
  *******************************************************************************/
 package io.glutamate.util.function;
 
+import java.util.Objects;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
+import java.util.function.Function;
 import java.util.function.IntSupplier;
 import java.util.function.LongSupplier;
 import java.util.function.Supplier;
@@ -27,32 +29,94 @@ public final class Suppliers {
     private Suppliers() {
     }
 
+    /**
+     * Create a supplier returning always the provided value.
+     *
+     * @param value
+     *            the value to return
+     * @return the supplier providing the value
+     */
     public static <@Nullable T> Supplier<T> constant(final T value) {
         return () -> value;
     }
 
+    /**
+     * Create a supplier returning always the provided value.
+     *
+     * @param value
+     *            the value to return
+     * @return the supplier providing the value
+     */
     public static IntSupplier constant(final int value) {
         return () -> value;
     }
 
+    /**
+     * Create a supplier returning always the provided value.
+     *
+     * @param value
+     *            the value to return
+     * @return the supplier providing the value
+     */
     public static LongSupplier constant(final long value) {
         return () -> value;
     }
 
+    /**
+     * Create a supplier returning always the provided value.
+     *
+     * @param value
+     *            the value to return
+     * @return the supplier providing the value
+     */
     public static DoubleSupplier constant(final double value) {
         return () -> value;
     }
 
+    /**
+     * Create a supplier returning always the provided value.
+     *
+     * @param value
+     *            the value to return
+     * @return the supplier providing the value
+     */
     public static BooleanSupplier constant(final boolean value) {
         return () -> value;
     }
 
+    /**
+     * Create a supplier returning always {@code true}.
+     *
+     * @return the supplier providing {@code true}
+     */
     public static BooleanSupplier ofTrue() {
         return () -> true;
     }
 
+    /**
+     * Create a supplier returning always {@code false}.
+     *
+     * @return the supplier providing {@code false}
+     */
     public static BooleanSupplier ofFalse() {
         return () -> false;
+    }
+
+    /**
+     * Provide a {@link Supplier} mapping values of another {@link Supplier}.
+     *
+     * @param supplier
+     *            the supply to provide original values
+     * @param function
+     *            the mapper function
+     * @return a supplier providing values of the original supplier, mapped by the
+     *         mapping function
+     */
+    public static <T, M> Supplier<M> map(final Supplier<T> supplier, final Function<T, M> function) {
+        Objects.requireNonNull(supplier);
+        Objects.requireNonNull(function);
+
+        return () -> function.apply(supplier.get());
     }
 
 }
